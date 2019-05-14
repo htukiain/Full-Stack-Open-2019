a29199c1caa14fc02271dce9dec3b49fc0650d1b
Heikki T läppäri <heikki.tukiainen@gmail.com>
2019-05-14 20:28:50 +0300

    true 1.5 version

--- osa1/kurssintiedot/src/index.js
+++ osa1/kurssintiedot/src/index.js
@@ -33,21 +33,25 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
-    const part1 = {
-      name: 'Reactin perusteet',
-      exercises: 10
-    }
-    const part2 = {
-      name: 'Tiedonvälitys propseilla',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'Komponenttien tila',
-      exercises: 14
-    }
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
 
-const parts = [part1, part2, part3];
+const parts = course;
 console.log(parts);
 
   return (
