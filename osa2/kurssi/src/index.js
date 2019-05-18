4ac17d4a752e586cd760ccc7a49bd8d035604941
Heikki T läppäri <heikki.tukiainen@gmail.com>
2019-05-18 08:49:55 +0300

    2.5: erillinen moduuli

--- osa2/kurssi/src/index.js
+++ osa2/kurssi/src/index.js
@@ -1,54 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-
-const Course = (props) => {
-
-    return (
-      <div>
-      <Header name ={props.course.name} />
-      <Content courses= {props.course.parts}/>
-      <Footer courses = {props.course.parts} />
-      </div>
-    )
-}
-
-const Header = (props) => {
-  return (
-      <h1>{props.name}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-
-    props.courses.map (part  =>
-      <Part key={part.id}
-      name={part.name} 
-      exercises={part.exercises}
-      />)
-
-  )
-
-
-
-}
- 
-const Part = (props) => {
-  return ( <div>{props.name} {props.exercises}</div>)
- 
-}
-
-const Footer = (props) => {
-
-  const reducer = (accumulator, currentValue) => accumulator + currentValue.exercises;
-  
-  return (
-
-    <div> Yhteensä  {props.courses.reduce(reducer,0)} tehtävää </div>
-
-  )
-}
+import Course from './components/Course'
 
 
 const App = () => {
@@ -94,7 +46,7 @@ const App = () => {
 
   return (
     <div>
-     { courses.map( item => <Course course={item} /> )}
+     { courses.map( item => <Course key = {item.id} course={item} /> )}
     </div>
   )
 }
